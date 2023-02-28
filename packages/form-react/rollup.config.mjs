16c19d4774e7be00c93f95fa0dca3f381f2dd7c9
闲人 <code@idler8.com>
2023-02-28 09:40:09 +0800

    ci: 增加rollup-plugin-dts

--- packages/form-react/rollup.config.mjs
+++ packages/form-react/rollup.config.mjs
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-export default {
+import dts from 'rollup-plugin-dts'
+export default [{
   cache: false,
   input: "libs/babel/index.js",
   output: {
@@ -8,4 +9,14 @@ export default {
     format: 'umd'
   },
   plugins: [nodeResolve({ modulePaths: ['libs/babel'], moduleDirectories: [] })]
-};
+}, {
+  input: "libs/typings/index.d.ts",
+  output: [{ file: "libs/rollup/index.d.ts", format: "es" }],
+  plugins: [
+    nodeResolve({
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json", '.d.ts'],
+      modulePaths: ['libs/typings'],
+      moduleDirectories: []
+    }),
+    dts()],
+}];
