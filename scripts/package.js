ca01ffdf67d6858e743d0a1de6ad5d9ed6c44dcf
闲人 <code@idler8.com>
2023-02-28 10:05:01 +0800

    chore: 给npm增加更多说明

--- scripts/package.js
+++ scripts/package.js
@@ -1,18 +1,44 @@
 const fs = require('fs');
 const path = require('path');
 const version = (process.env.GITHUB_REF || 'v0.0.1-0').replace("refs/tags/", "");
-const corePackageTarget = path.resolve(__dirname, '../packages/observer/libs/rollup/package.json')
-fs.writeFileSync(corePackageTarget, JSON.stringify({
-  name: "@idler8/observer",
-  version: version,
-  main: "./index.js",
-}))
-const formReactPackageTarget = path.resolve(__dirname, '../packages/form-react/libs/rollup/package.json')
-fs.writeFileSync(formReactPackageTarget, JSON.stringify({
-  name: "@idler8/form-react",
-  version: version,
-  main: "./index.js",
-  "dependencies": {
+function makePackageFile(project, packageJson) {
+  const npmPublishDir = path.resolve(__dirname, '../packages/' + project + '/libs/rollup')
+  fs.writeFileSync(path.resolve(npmPublishDir, 'package.json'), JSON.stringify({
+    name: "@idler8/" + project,
+    version: version,
+    main: "./index.js",
+    author: {
+      name: "闲人",
+      email: 'code@idler8.com'
+    },
+    homepage: "https://github.com/idler8/observer",
+    repository: {
+      "type": "git",
+      "url": "https://github.com/idler8/observer"
+    },
+    ...packageJson
+  }))
+  fs.writeFileSync(path.resolve(npmPublishDir, 'README.md'), `
+  ## 该项目来自Github Actions自动构建
+  更多说明请查看[Github仓库]("https://github.com/idler8/observer")
+  `)
+}
+makePackageFile('observer', {
+  keywords: [
+    "observer",
+    "events",
+    "listener"
+  ],
+})
+makePackageFile('form-react', {
+  keywords: [
+    "observer",
+    "react",
+    "form",
+    "field",
+    "input"
+  ],
+  dependencies: {
     "@idler8/observer": "~" + version
   },
-}))
+})
