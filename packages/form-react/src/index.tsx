17246cd8d4e80bf22a561a1f881497dfe13827fd
闲人 <code@idler8.com>
2023-03-04 01:20:30 +0800

    feat: 增加forwardForm函数，快捷建立表单上下文

--- packages/form-react/src/index.tsx
+++ packages/form-react/src/index.tsx
@@ -16,6 +16,7 @@ export {
 };
 import { Valid } from "Validator/index";
 import type { Key, Keys } from "FKeys/index";
+import { forwardRef } from "react";
 export type { Key, Keys, Valid };
 export function FormValidator({ children }) {
   return (
@@ -29,4 +30,23 @@ export function useFieldState(keys?: Keys, validator?: Valid) {
   const onChange = useFieldTrigger(keys);
   const errResponse = useRuleValidator(validator, keys);
   return [value, onChange, errResponse];
+}
+export function forwardForm(Component, hasValidator) {
+  return hasValidator
+    ? function FormValidatorForward(props) {
+        return (
+          <Form>
+            <Validator>
+              <Component {...props} />
+            </Validator>
+          </Form>
+        );
+      }
+    : function FormForward(props) {
+        return (
+          <Form>
+            <Component {...props} />
+          </Form>
+        );
+      };
 }
