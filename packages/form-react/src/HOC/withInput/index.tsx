fa19087973b556d2d6d5c047a1bc5eda0a9bbcad
闲人 <code@idler8.com>
2023-03-16 10:19:28 +0800

    feat: hoc中继承FKey

--- packages/form-react/src/HOC/withInput/index.tsx
+++ packages/form-react/src/HOC/withInput/index.tsx
@@ -1,17 +1,17 @@
 import { Callback } from "@idler8/observer";
-import { useFieldState } from "index";
+import { useFieldState, useKeys } from "index";
+import type { Valid } from "index";
 import type { ComponentType, FunctionComponent } from "react";
 export default function withInput<InputProps>(
   Component: ComponentType<
     { value?: string; onChange?: Callback; validateMessage?: any } & InputProps
   >
-): FunctionComponent<{ name?: string; rule?: Function } & InputProps> {
-  return function Wapper(props: InputProps): JSX.Element {
-    if (props["name"] === undefined) return <Component {...props} />;
-    const [value, onChange, validateMessage] = useFieldState(
-      props["name"],
-      props["rule"]
-    );
+): FunctionComponent<{ name?: string; rule?: Valid } & InputProps> {
+  return function Wapper(props): JSX.Element {
+    const { name, rule } = props;
+    if (name === undefined) return <Component {...props} />;
+    const keys = useKeys(name);
+    const [value, onChange, validateMessage] = useFieldState(keys, rule);
     return (
       <Component
         value={value}
